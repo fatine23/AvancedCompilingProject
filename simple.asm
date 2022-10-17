5feb1e7e4f0409d779fef84a3bcd87c79fca64a9
Sebastian Rojas <jhsrojasro@unal.edu.co>
2022-10-17 11:28:45 +0200

    code du prof

---
+++ simple.asm
@@ -0,0 +1,27 @@
+extern printf
+section.data 
+hello : db"helle world %d" ,10, 0
+fmt :db "%s" , 10, 0
+argc :dq 0
+argv : dq 0
+
+section .text
+globale main 
+main: 
+    push rbp
+    mov[argc], rdi
+    mov[argv], rsi
+    mov rdi , hello 
+    mov rsi , [argc] ; "rsi=42"
+    call printf 
+    mov rdi , fmt 
+    mov rbx , [argv]
+    mov rdi , [rbx+8]
+    call atoi 
+    mov rdi , hello 
+    mov rsi , rax
+    call printf 
+    pop rbp 
+    ret
+
+    
